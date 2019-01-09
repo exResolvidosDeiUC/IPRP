2b44cab9cc6e868abc8d8b3c2a62dd78195808f1
exResolvidosDeiUC <46534647+exResolvidosDeiUC@users.noreply.github.com>
2019-01-09 19:07:18 +0000

    Update Normal2006-2007-ex5.py

--- 2006-2007/Normal2006-2007-ex5.py
+++ 2006-2007/Normal2006-2007-ex5.py
@@ -1,4 +1,4 @@
 def produto_escalar(v1, v2):
-    return sum(v1[i] * v2[1] for i in range(len(v1)))
+    return sum(v1[i] * v2[i] for i in range(len(v1)))
 
 print(produto_escalar((1, 2, 3), (0, 1, 2)))
