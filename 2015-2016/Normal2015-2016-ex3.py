293425d2c7beb10c82184ce349a8877882cca1e4
exResolvidosDeiUC <46534647+exResolvidosDeiUC@users.noreply.github.com>
2019-01-09 23:00:16 +0000

    Update Normal2015-2016-ex3.py

--- 2015-2016/Normal2015-2016-ex3.py
+++ 2015-2016/Normal2015-2016-ex3.py
@@ -1,7 +1,7 @@
 def conta(cad, sub_cad):
     cnt = 0
-    for i in range(len(cad)-len(sub_cad)+1):
-        if cad[i:i+len(sub_cad)] == sub_cad:
-            cnt+=1
+    for i in range(len(cad)-len(sub_cad)+1): #numero de sub-cadeias a ser testadas
+        if cad[i:i+len(sub_cad)] == sub_cad: #fatiamento da dimensão de sub_cad
+            cnt+=1 #encontrando uma ocorrência, incrementa
     return cnt
 print(conta('olalal mundo', 'lal'))
