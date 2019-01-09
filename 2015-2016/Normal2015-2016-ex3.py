7b57df016bed753a6c271c4d85059769c242e9e8
exResolvidosDeiUC <46534647+exResolvidosDeiUC@users.noreply.github.com>
2019-01-09 22:51:02 +0000

    Update Normal2015-2016-ex3.py

--- 2015-2016/Normal2015-2016-ex3.py
+++ 2015-2016/Normal2015-2016-ex3.py
@@ -1,9 +1,7 @@
 def conta(cad, sub_cad):
-    pos = [[1,2,3]]
-    for i in range(len(cad)):        
-        try:
-            if cad[i:i + len(sub_cad)] == sub_cad and list(range(i, i+len(sub_cad))) not in [(j for j in i) for i in pos]:
-                pos.append(ist(range(i, i+len(sub_cad))))
-        except:
-            pass
-    return len(pos)    
+    cnt = 0
+    for i in range(len(cad)-len(sub_cad)+1):
+        if cad[i:i+len(sub_cad)] == sub_cad:
+            cnt+=1
+    return cnt
+print(conta('olalal mundo', 'lal'))
