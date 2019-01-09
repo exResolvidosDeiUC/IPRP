98271060b79ef81302eb504fce0d203b81347ebd
exResolvidosDeiUC <46534647+exResolvidosDeiUC@users.noreply.github.com>
2019-01-09 19:10:53 +0000

    Update Normal2006-2007-ex5.py

--- 2006-2007/Normal2006-2007-ex5.py
+++ 2006-2007/Normal2006-2007-ex5.py
@@ -1,4 +1,9 @@
 def produto_escalar(v1, v2):
-    return sum(v1[i] * v2[i] for i in range(len(v1)))
+    soma = 0
+    for i in range(len(v1)):
+        soma += v1[i] * v2[i]
+    return soma
+    #abaixo, solução em 1 só linha
+    #return sum(v1[i] * v2[i] for i in range(len(v1)))
 
 print(produto_escalar((1, 2, 3), (0, 1, 2)))
